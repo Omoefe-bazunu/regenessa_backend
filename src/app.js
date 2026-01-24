f5d7b9ccfbe11777ec471477e8f51b55f040218e
omoefe <raniem57@gmail.com>
2026-01-24 16:52:07 +0100

    at

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ const app = express();
 app.use(express.json({ limit: "150mb" }));
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000", "https://cleanfoods.vercel.app"],
+    credentials: true,
   }),
 );
 
