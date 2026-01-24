0b2b0dc8aa8f7f10c51df3e16c1410814f732d57
omoefe <raniem57@gmail.com>
2026-01-24 16:56:52 +0100

    pp

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "https://cleanfoods.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
